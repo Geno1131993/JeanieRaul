952facec9e9c32e0ce7625b8a5bb85a11bf545c0
Danielle <ukka.ukka.nuka.nuka.appu.wa@gmail.com>
2020-09-07 00:23:49 -0700

    Updated visuals, added questions and images

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,24 +1,78 @@
 function populate_icons(){
     //Carpet cleaning
-    $("#carpets").append(image("Carpets"));
+    $("#carpets").append(image_button("Carpets"));
 
     //Cleaning Services
-    $("#houses").append(image("Cleaning"));
+    $("#houses").append(image_button("Cleaning"));
 
     //Maintenance
-    $("#maintenance").append(image("Hammer"));
+    $("#maintenance").append(image_button("Hammer"));
 
     //Painting
-    $("#painting").append(image("Paint"));
+    $("#painting").append(image_button("Paint"));
+
+    let overview_text = `
+        Welcome to Jeanie and Raul's Maintenance and Carpet Cleaning Services!</br></br>
+
+        We've been family-owned and operated in Josephine County since 1995. We are skilled licensed and bonded professionals, offering services in carpet cleaning, move-out cleaning of vacant units, indoor and outdoor painting, and maintenance services.
+    `;
 
 
-    console.log("From script.");
+    let questions_text = `
+        </br></br>
+        Have questions about our services? Need a quote?</br></br>
+        Call us to ask for a quote or a breakdown of the services we offer!
+    `;
+
+    $("#overview_text").append(text(overview_text));
+    $("#overview").append(card("Raul Orozco", "(541)474-1334", "Owner"));
+    $("#overview").append(card("Jeanie Orozco", "(541)474-1334", "Owner"));
+
+    $("#questions").append(image_icon("Question"));
+    $("#questions").append(text(questions_text));
 
 }
 
 
-function image(name){
+function image_button(name){
+    return `<img src = "./assets/images/${name}.png" class = "button-image">`;
+}
+
+
+
+function image_icon(name){
     return `<img src = "./assets/images/${name}.png" class = "icon-image">`;
+
+}
+
+
+
+function image_jpg(name){
+    return `<img src = "./assets/images/${name}.jpg" class = "card-image">`;
+}
+
+
+function text(body){
+    return `<p> ${body} </p>`;
+}
+
+
+
+function card(name, phone, title){
+    return `
+    <div class = "card">
+        <p class = "card_name">${name}</p>
+        <div class = "tile is-horizontal">
+            <div class = "tile is-4">
+                ${image_jpg(name)}
+            </div>
+            <div class = "article is-8">
+                <p class = "card_text" id = "position"> ${title} </p>
+                <p class = "card_text" id = "phone"> ${phone} </p>
+            </div>
+        </div>
+    </div>
+    `;
 }
 
 
